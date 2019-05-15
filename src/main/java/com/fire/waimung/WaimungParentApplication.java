a5d1094fd7fb59553425e3a375bb0941cac552d2
wanggq1109 <wgq4612@gmail.com>
2019-05-15 16:14:55 +0800

    微服务基础架构

---
+++ src/main/java/com/fire/waimung/WaimungParentApplication.java
@@ -0,0 +1,13 @@
+package com.fire.waimung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaimungParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaimungParentApplication.class, args);
+    }
+
+}
